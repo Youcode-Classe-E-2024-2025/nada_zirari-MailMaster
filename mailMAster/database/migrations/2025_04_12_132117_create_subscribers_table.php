b31135d2d1c1716ddb174d23e47d6484b5f07766
Nada Zirari <zirarinada20@gmail.com>
2025-04-12 14:22:38 +0100

    lajout du function up in migration of subscriber

--- mailMAster/database/migrations/2025_04_12_132117_create_subscribers_table.php
+++ mailMAster/database/migrations/2025_04_12_132117_create_subscribers_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('subscribers', function (Blueprint $table) {
             $table->id();
+            $table->string('email');
+            $table->foreignId('newsletter_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
