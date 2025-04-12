e358e29359ebaa5f211a594b83f63ad4c5eb5a73
Nada Zirari <zirarinada20@gmail.com>
2025-04-12 14:36:46 +0100

    doccumenter avec swager le authcontroller

--- mailMAster/app/Http/Controllers/AuthController.php
+++ mailMAster/app/Http/Controllers/AuthController.php
@@ -6,11 +6,44 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
-
+/**
+ * @OA\Info(
+ *    title="MailMAster API",
+ *    version="1.0.0",
+ * )
+ * @OA\Tag(
+ *     name="Authentication",
+ *     description="API Endpoints for authentication"
+ * )
+ * @OA\SecurityScheme(
+ *     type="http",
+ *     scheme="bearer",
+ *     bearerFormat="JWT",
+ *     securityScheme="bearerAuth"
+ * )
+ */
 class AuthController extends Controller
 {
 
-  
+   /**
+     * @OA\Post(
+     *     path="/api/register",
+     *     tags={"Authentication"},
+     *     summary="Register a new user",
+     *     @OA\RequestBody(
+     *         @OA\JsonContent(
+     *             required={"name","email","password"},
+     *             @OA\Property(property="name", type="string"),
+     *             @OA\Property(property="email", type="string"),
+     *             @OA\Property(property="password", type="string")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=201,
+     *         description="User registered",
+     *     )
+     * )
+     */
     public function register(Request $request)
     {
         try {
@@ -37,7 +70,24 @@ class AuthController extends Controller
         }
     }
 
-    
+    /**
+     * @OA\Post(
+     *     path="/api/login",
+     *     tags={"Authentication"},
+     *     summary="User login",
+     *     @OA\RequestBody(
+     *         @OA\JsonContent(
+     *             required={"email","password"},
+     *             @OA\Property(property="email", type="string"),
+     *             @OA\Property(property="password", type="string")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Login successful"
+     *     )
+     * )
+     */
     public function login(Request $request)
     {
         try {
@@ -62,7 +112,18 @@ class AuthController extends Controller
             return response()->json(['error' => $e->getMessage()], 500);
         }
     }
-
+ /**
+     * @OA\Post(
+     *     path="/api/logout",
+     *     tags={"Authentication"},
+     *     summary="User logout",
+     *     security={{ "bearerAuth": {} }},
+     *     @OA\Response(
+     *         response=200,
+     *         description="Logout successful"
+     *     )
+     * )
+     */
     
     public function logout(Request $request)
     {
