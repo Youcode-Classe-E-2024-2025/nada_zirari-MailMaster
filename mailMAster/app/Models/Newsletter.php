af66142d496788418bb8624fb5a718dd9e280b96
Nada Zirari <zirarinada20@gmail.com>
2025-04-12 14:19:54 +0100

    lajout du fillable of newsletter

--- mailMAster/app/Models/Newsletter.php
+++ mailMAster/app/Models/Newsletter.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Newsletter extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'content'
+    ];
+
+    public function subscribers()
+    {
+        return $this->hasMany(Subscriber::class);
+    }
+
+    public function campaigns()
+    {
+        return $this->hasMany(Campaign::class);
+    }
 }
