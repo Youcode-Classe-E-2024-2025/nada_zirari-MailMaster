174198e9ee011a2f33b36082654452a98790f500
Nada Zirari <zirarinada20@gmail.com>
2025-04-12 14:17:30 +0100

    lajout du fillable necsesaire

--- mailMAster/app/Models/Campaign.php
+++ mailMAster/app/Models/Campaign.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Campaign extends Model
 {
-    //
+    protected $fillable = [
+        'subject',
+        'content',
+        'newletter_id'
+    ];
+
+    public function newsletter()
+    {
+        return $this->belongsTo(Newsletter::class);
+    }
 }
